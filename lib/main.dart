0e14f69ca70a16dc92c929b3962f7711d30c9922
Tabassum Khanum <66406315+TabassumKhanum@users.noreply.github.com>
2020-06-13 20:54:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      //color: Colors.green,
+      color: Colors.green,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black),shape: BoxShape.circle,
-        color: Colors.red
+        //color: Colors.red
       ),
     );
   }
